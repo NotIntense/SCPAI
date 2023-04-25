e6c9d4711a7d81d453cbc2a5147243ceb6e94845
NotIntense <mbro8210@gmail.com>
2023-04-25 09:56:21 -0400

    Update Info

--- SCPAI/Properties/AssemblyInfo.cs
+++ SCPAI/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("NotIntense")]
 [assembly: AssemblyProduct("SCPAI")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyCopyright("Copyright NotIntense©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
